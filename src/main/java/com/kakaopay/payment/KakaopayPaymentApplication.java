5ad756c84186d8625667ffdafa8b95ddf6ad7261
ctp102 <ctp102>
2023-06-17 13:34:12 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/kakaopay/payment/KakaopayPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaopayPaymentApplication.class, args);
+    }
+
+}
